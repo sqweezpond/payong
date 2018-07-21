a909fdfce97e9e868563c1ae7aaa727d401aef5d
Parinya Yodming <sqweezpond>
2018-07-21 14:53:28 +0700

    update
    update (pond)

---
+++ inc/navbar.php
@@ -0,0 +1,70 @@
+
+
+<!--- NAVBAR 1 ------------------------------------------------->
+	 	 <div class="container">
+					<div class="row nav-m-t">
+							<div class="col-xl-6 col-lg-7 col-md-9 col-9">
+							   <div class="row">
+										 <div class="col-xl-6 col-lg-6 col-md-5 col-6 px-0">
+												<div class="nav-overlay-image nav-position-r">
+													<a href="index.php">
+														<img src="assets/images/logo/logo-payong.jpg" class="nav-image-full">
+													</a>
+												</div>
+										 </div>
+									</div>
+									<div class="row mx-0">
+										<div class="col-12 nav-p-lr">
+											<div class="nav-font3 nav-position-a">อาจารย์พยงค์ ประสบการณ์โหราศาสตร์ ๔๐ ปี</div>
+										</div>
+									</div>
+							</div>
+							<div class="col-xl-6 col-lg-5 col-md-3 col-3 text-right nav-p-t2">
+									<span class="">
+											<a href="" class="nav-font1">TH</a>
+									</span>
+									<span class="nav-font2">:</span>
+									<span class="">
+										<a href="" class="nav-font1">EN</a>
+									</span>
+							</div>
+					</div>
+			</div>
+<!-- END NAVBAR 1-------------------------------------->
+
+
+<!-- NAVBAR 2 ---------------------------------------------------->
+<div class="nav-bg">
+		<div class="container">
+				<nav class="navbar navbar-expand-lg nav-p-t px-0">
+							<button class="navbar-toggler nav-hamburger" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+									<i class="fa fa-bars" aria-hidden="true"></i>
+							</button>
+							<div class="col-md-6 col-6 text-right d-block d-lg-none d-md-block px-0">
+									<button type="button" class="btn nav-btn">แจ้งโอนเงิน</button>
+							</div>
+							<div class="col-xl-9 col-lg-9 px-0 pl-2">
+							<div class="collapse navbar-collapse nav-bg2" id="navbarNavDropdown">
+									<ul class="navbar-nav">
+											<li class="nav-item active">
+													<a class="nav-link nav-font4" href="#">หน้าแรก</a>
+											</li>
+											<li class="nav-item">
+													<a class="nav-link nav-font4" href="#">ดวงชะตา</a>
+											</li>
+											<li class="nav-item">
+													<a class="nav-link nav-font4" href="#">ฤกษ์คนไทยในประเทศ</a>
+											</li>
+											<li class="nav-item">
+													<a class="nav-link nav-font4" href="#">ฤกษ์คนไทยในต่างประเทศ</a>
+											</li>
+									</ul>
+							</div>
+						</div>
+						<div class="col-xl-3 col-lg-3 text-right d-none d-lg-block d-md-none px-0">
+								<button type="button" class="btn nav-btn">แจ้งโอนเงิน</button>
+						</div>
+				</nav>
+		</div>
+</div>
+<!-- END NAVBAR 2 ---------------------------->
